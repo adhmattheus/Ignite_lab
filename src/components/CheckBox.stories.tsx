0506700b749c0f64e9ac239c765a52096d8c6e65
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-10-26 12:18:13 -0300

    checkbox finalizado

---
+++ src/components/CheckBox.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { CheckBox, CheckBoxProps } from './CheckBox'
+
+export default {
+  title: 'Components/CheckBox',
+  component: CheckBox,
+  args: {
+  },
+} as Meta<CheckBoxProps>
+
+export const Default: StoryObj<CheckBoxProps> = {}
